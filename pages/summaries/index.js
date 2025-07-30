8c29b4136bd92fd38fda8b926c4cf6774fb7faf4
TheGainMaker <matthew_171@msn.com>
2025-07-29 20:31:30 -0400

    Create index.js

---
+++ pages/summaries/index.js
@@ -0,0 +1,18 @@
+import Link from "next/link";
+
+export default function SummariesIndex() {
+  return (
+    <div className="max-w-2xl mx-auto px-4 py-8">
+      <h1 className="text-4xl font-bold mb-6">Book Summaries</h1>
+
+      <ul className="list-disc list-inside space-y-4">
+        <li>
+          <Link href="/summaries/atomic-habits" className="text-blue-600 hover:underline">
+            Atomic Habits by James Clear
+          </Link>
+        </li>
+        {/* You can add more summaries here later */}
+      </ul>
+    </div>
+  );
+}
