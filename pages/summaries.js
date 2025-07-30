c9eb3d4ac881ab7c3c80c6ad3f526469797aff2d
TheGainMaker <matthew_171@msn.com>
2025-07-29 20:12:37 -0400

    Update summaries.js

--- pages/summaries.js
+++ pages/summaries.js
@@ -59,3 +59,21 @@ export default function Summaries() {
     </div>
   );
 }
+import Link from "next/link";
+
+export default function SummariesIndex() {
+  return (
+    <div className="max-w-2xl mx-auto px-4 py-8">
+      <h1 className="text-4xl font-bold mb-6">Book Summaries</h1>
+
+      <ul className="list-disc list-inside space-y-4">
+        <li>
+          <Link href="/summaries/atomic-habits" className="text-blue-600 hover:underline">
+            Atomic Habits by James Clear
+          </Link>
+        </li>
+        {/* Add more summaries here */}
+      </ul>
+    </div>
+  );
+}
