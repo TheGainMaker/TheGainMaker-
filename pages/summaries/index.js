a8b68394124f16b290926e05979987bf23dc55c0
TheGainMaker <matthew_171@msn.com>
2025-07-30 21:06:29 -0400

    Update index.js

--- pages/summaries/index.js
+++ pages/summaries/index.js
@@ -4,13 +4,12 @@ export default function SummariesIndex() {
   return (
     <div className="max-w-2xl mx-auto px-4 py-8">
       <h1 className="text-4xl font-bold mb-6">Book Summaries</h1>
-
       <ul className="list-disc list-inside space-y-4">
         <li>
-          <Link href="/summaries/atomic-habits">Atomic Habits</Link>
-          <li><Link href="/summaries/deep-work">Deep Work by Cal Newport</Link></li>
+          <Link href="/summaries/atomic-habits" className="text-blue-600 hover:underline">
+            Atomic Habits by James Clear
+          </Link>
         </li>
-        {/* You can add more summaries here later */}
       </ul>
     </div>
   );
