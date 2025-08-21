e23f31a2faf064b7fad7c5b62b5c80fcc081e137
TheGainMaker <matthew_171@msn.com>
2025-08-20 20:01:37 -0400

    Delete pages/investment - strategy directory

--- pages/investment - strategy/index.js
+++
@@ -1,8 +0,0 @@
-export default function InvestmentStrategy() {
-  return (
-    <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
-      <h1>Investment Strategy</h1>
-      <p>This section covers smart strategies for building and preserving wealth.</p>
-    </div>
-  );
-}
